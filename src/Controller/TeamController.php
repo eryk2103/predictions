bdf9d6821881546ea10279f295d5503b2e8c6f16
Eryk <eryk9903@gmail.com>
2026-05-28 15:37:13 +0200

    create team crud controller and service

---
+++ src/Controller/TeamController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Team;
+use App\Form\TeamType;
+use App\TeamService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/team')]
+class TeamController extends AbstractController
+{
+    public function __construct(private readonly TeamService $service) {}
+
+    #[Route('', name: 'team_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('team/index.twig', [
+            'teams' => $this->service->getAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'team_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $team = new Team();
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->service->create(
+                $team->getName(),
+                $team->getShortName(),
+                $team->getCode(),
+                $team->getLogo() ?? '',
+                $team->getCity(),
+                $team->getCountry(),
+                $team->getStadium(),
+            );
+
+            return $this->redirectToRoute('team_index');
+        }
+
+        return $this->render('team/new.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'team_show', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('team/show.twig', [
+            'team' => $this->service->get($id),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'team_edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, Request $request): Response
+    {
+        $team = $this->service->get($id);
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->service->update(
+                $team,
+                $team->getName(),
+                $team->getShortName(),
+                $team->getCode(),
+                $team->getLogo(),
+                $team->getCity(),
+                $team->getCountry(),
+                $team->getStadium(),
+            );
+
+            return $this->redirectToRoute('team_show', ['id' => $id]);
+        }
+
+        return $this->render('team/edit.twig', [
+            'team' => $team,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'team_delete', methods: ['POST'])]
+    public function delete(int $id, Request $request): Response
+    {
+        $team = $this->service->get($id);
+
+        if ($this->isCsrfTokenValid('delete_team_' . $id, $request->getPayload()->getString('_token'))) {
+            $this->service->delete($team);
+        }
+
+        return $this->redirectToRoute('team_index');
+    }
+}
---
+++ src/TeamService.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App;
+
+use App\Entity\Country;
+use App\Entity\Stadium;
+use App\Entity\Team;
+use App\Repository\TeamRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class TeamService
+{
+    public function __construct(
+        private readonly TeamRepository $repository,
+        private readonly EntityManagerInterface $em,
+    ) {}
+
+    public function create(
+        string $name,
+        string $shortName,
+        string $code,
+        string $logo,
+        ?string $city = null,
+        ?Country $country = null,
+        ?Stadium $stadium = null,
+    ): Team {
+        $team = (new Team())
+            ->setName($name)
+            ->setShortName($shortName)
+            ->setCode($code)
+            ->setLogo($logo)
+            ->setCity($city)
+            ->setCountry($country)
+            ->setStadium($stadium);
+
+        $this->em->persist($team);
+        $this->em->flush();
+
+        return $team;
+    }
+
+    public function get(int $id): Team
+    {
+        $team = $this->repository->find($id);
+
+        if ($team === null) {
+            throw new NotFoundHttpException(sprintf('Team %d not found.', $id));
+        }
+
+        return $team;
+    }
+
+    /** @return Team[] */
+    public function getAll(): array
+    {
+        return $this->repository->findAll();
+    }
+
+    public function update(
+        Team $team,
+        ?string $name = null,
+        ?string $shortName = null,
+        ?string $code = null,
+        ?string $logo = null,
+        ?string $city = null,
+        ?Country $country = null,
+        ?Stadium $stadium = null,
+    ): Team {
+        if ($name !== null) {
+            $team->setName($name);
+        }
+        if ($shortName !== null) {
+            $team->setShortName($shortName);
+        }
+        if ($code !== null) {
+            $team->setCode($code);
+        }
+        if ($logo !== null) {
+            $team->setLogo($logo);
+        }
+        if ($city !== null) {
+            $team->setCity($city);
+        }
+        if ($country !== null) {
+            $team->setCountry($country);
+        }
+        if ($stadium !== null) {
+            $team->setStadium($stadium);
+        }
+
+        $this->em->flush();
+
+        return $team;
+    }
+
+    public function delete(Team $team): void
+    {
+        $this->em->remove($team);
+        $this->em->flush();
+    }
+}
